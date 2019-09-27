c02f4224e07c484af0419345d9cc3f4b1a1d426c
Michal Drygajlo <dryguz@gmail.com>
2019-09-27 17:50:00 +0200

    created script splitting data by date and hour updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # kaggle-avazu
-kaggle-avazu
 
-exercise project - implementation of TFX pipeline
+Exercise project for testing
+
+Data are provided in one big csv file. 
+"prepare_data.py" splits the data by day and by hour.
+The results are folders with parquet files.
+
+Folder's schema is:
+"data=xxxx-xx-xx"/"hour=xx"/"parqets_files_with_random_string_name".
+
+To run the script use command:
+```
+python prepare_data.py --file train.csv
+```
---
+++ prepare_data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import random
+import string
+import os
+import pyarrow
+import argparse
+
+
+def random_string(string_length=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(string_length))
+
+
+def split_date_file(file):
+    file_path = os.path.join('data', file)
+    reader = pd.read_csv(file_path, chunksize=30000)
+    for chunk in reader:
+        chunk.hour = pd.to_datetime(chunk.hour, format='%y%m%d%H')
+        grouped_sample = chunk.groupby('hour')
+        for name, sample in grouped_sample:
+            date = 'date={}'.format(name.date())
+            hour = 'hour={:02d}'.format(name.hour)
+            path = os.path.join('data', 'split_by_date', date, hour)
+            if not os.path.isdir(path):
+                os.makedirs(path)
+            name = random_string(10)
+            file_path = os.path.join(path, name)
+            sample.to_parquet(fname=file_path, engine='pyarrow', compression='snappy')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get file name in data folder')
+    parser.add_argument('--file', type=str, help='provide a name of file in data folder to split by date')
+    args = parser.parse_args()
+
+    file = args.file
+    split_date_file(file)
