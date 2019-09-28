0f4f4e0713bcb5d8f344cb85dcabc0d76fe95526
Michal Drygajlo <dryguz@gmail.com>
2019-09-28 09:07:31 +0200

    [done] simulate_streaming completed README.md updated

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 Exercise project for testing
 
+##Data preparation
 Data are provided in one big csv file. 
-"prepare_data.py" splits the data by day and by hour.
+[File description under this link](https://www.kaggle.com/c/avazu-ctr-prediction/data)
+###prepare_data.py
+Script splits the data by day and by hour.
 The results are folders with parquet files.
 
 Folder's schema is:
@@ -13,3 +16,10 @@ To run the script use command:
 ```
 python prepare_data.py --file train.csv
 ```
+
+###simulate_streamin.py 
+Script simulate streaming by doing in a while loop, every x seconds:
+- checking content of root_folder = 'data/split_by_date/'
+- checking content of input_folder = 'data/input_data/'
+- comparing two lists and taking the earliest day and hour from not copied yet
+- copies one hour of data
--- simulate_streaming.py
+++ simulate_streaming.py
@@ -2,6 +2,7 @@ import os
 import time
 import datetime
 import argparse
+import shutil
 
 
 def list_days_and_hours(folder):
@@ -13,9 +14,8 @@ def list_days_and_hours(folder):
     days_and_hours = []
 
     for day in days:
-        day_path = os.path.join(folder, day)
-        hours = os.listdir(day_path)
-        days_and_hours += [os.path.join(day_path, hour) for hour in hours]
+        hours = os.listdir(os.path.join(folder, day))
+        days_and_hours += [os.path.join(day, hour) for hour in hours]
     days_and_hours.sort()
 
     return days_and_hours
@@ -26,24 +26,31 @@ def copy_another_hour_of_data(root_folder, input_folder):
     input_days_and_hours = list_days_and_hours(input_folder)
     to_copy_list = list(set(root_days_and_hours) - set(input_days_and_hours))
     to_copy_list.sort()
+    src = os.path.join(root_folder, to_copy_list[0])
+    dst = os.path.join(input_folder, to_copy_list[0])
+    shutil.copytree(src=src, dst=dst)
     return to_copy_list[0]
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get number of seconds for copying next hour of input data')
-    parser.add_argument('--file', type=str, help='provide a time window in seconds for copying hour of data')
+    parser.add_argument('--tw', type=int, help='provide a time window in seconds for copying hour of data')
+    parser.add_argument('--sleep', type=int, help='how log to wait before check again if time window past')
     args = parser.parse_args()
 
-    time_window = args.file
+    time_window = args.tw
+    sleep = args.sleep
 
     t0 = datetime.datetime.now()
     while True:
         t1 = datetime.datetime.now()
         if (t1 - t0).seconds // time_window > 0:
-            copy_another_hour_of_data('data/split_by_date', 'data/input_data')
+            copied = copy_another_hour_of_data('data/split_by_date', 'data/input_data')
+            print('Copied folder {}'.format(copied))
             t0 = datetime.datetime.now()
         else:
-            time.sleep(600)
+            print('Sleeping for {} minutes, then checking again'.format(sleep/60))
+            time.sleep(sleep)
 
 
 
